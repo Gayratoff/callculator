acc0b5e0d577ccc89d0059eab6fb7284897443d1
Xudoberdi G'ayratov <115026746+Gayratoff@users.noreply.github.com>
2023-06-25 23:37:32 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='matem',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[''],
+    author='XUDOBERDI GAYRATOV',
+    author_email='xudoberdigayratov@gmail.com',
+    description='Description of my package',
+    url='https://github.com/Gayratoff/callculator.git',
+)
